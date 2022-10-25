6fe5ea000f90e86fbaba86d43af9274d885e6697
YevhenTar <58787169+YevhenTar@users.noreply.github.com>
2022-10-26 02:58:12 +0300

    Add slider effect cube

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 "use strict";
 
 const swiper = new Swiper(".mySwiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
     pagination: {
         el: ".swiper-pagination",
         dynamicBullets: true,
