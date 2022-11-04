85c0a8192c5a4fe80daccdfdeeecc2580350c752
YevhenTar <58787169+YevhenTar@users.noreply.github.com>
2022-11-04 15:38:59 +0200

    Add preloader to project

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 600);
+}
+
 const swiper = new Swiper(".mySwiper", {
     effect: "cube",
     grabCursor: true,
